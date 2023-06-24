b4350359d7a6a02366c9fb81354e1132c81f769e
orenfix10 <59178503+orenfix10@users.noreply.github.com>
2023-06-24 22:46:15 +0300

    Update viso.py

--- viso.py
+++ viso.py
@@ -23,7 +23,6 @@ df_grouped = df.groupby(['country', 'hotel']).size().reset_index(name='counts')
 df_pivot = df_grouped.pivot(index='country', columns='hotel', values='counts').reset_index().fillna(0)
 df_pivot['total'] = df_pivot['City Hotel'] + df_pivot['Resort Hotel']
 
-
 # Date range slider for selecting a specific time period
 date_range = st.slider("Select a Month Range", min_value=int(df['arrival_month'].min()),
                        max_value=int(df['arrival_month'].max()),
@@ -34,33 +33,30 @@ df_filtered = df[(df['arrival_month'] >= date_range[0]) & (df['arrival_month'] <
 
 # Add a selectbox for country selection
 country_options = ['All'] + list(df_filtered['country'].unique())
-selected_country = st.sidebar.selectbox('Select Country', options=country_options)
+selected_country = st.sidebar.selectbox('Select Country To Filter', options=country_options)
+st.sidebar.markdown(
+    "This filter enables you to select a country and view order data specific to that country. It updates the first 4 graphs to show visualizations tailored to the selected country and provides a data overview section with the option to adjust the number of displayed rows")
+st.sidebar.markdown("------------------")  # Add a horizontal line
 
 if selected_country != 'All':
     df_filtered = df_filtered[df_filtered['country'] == selected_country]
 
 # Add a selectbox for hotel type
-hotel_type = st.sidebar.selectbox('Select Hotel Type', options=['All'] + list(df_filtered['hotel'].unique()))
+hotel_type = st.sidebar.selectbox('Select Hotel Type To Filter', options=['All'] + list(df_filtered['hotel'].unique()))
+st.sidebar.markdown(
+    "Select a hotel type to filter the data and view order information specific to that type. The filter updates the first 4 graphs to show visualizations tailored to the selected hotel type and country, and provides a data overview section with adjustable row display.")
 if hotel_type != 'All':
     df_filtered = df_filtered[df_filtered['hotel'] == hotel_type]
 
-# Add a checkbox for whether to include cancelled bookings
-include_cancelled = st.sidebar.checkbox('Include Cancelled Bookings', value=True)
-if not include_cancelled:
-    df_filtered = df_filtered[df_filtered['is_canceled'] == 0]
-
 # Add a data overview section
-if st.sidebar.checkbox('Show Data Overview', value=True):
+if st.checkbox('Show Data Overview', value=True):
     st.subheader('Data Overview')
-
-
 
     # Add a number input field to select the number of rows
     num_rows = st.number_input('Enter number of rows', min_value=5, max_value=df_filtered.shape[0], value=5)
 
     st.write(df_filtered.head(num_rows))
     st.write(f"Number of rows: {df_filtered.shape[0]}")
-
 
 # Filter for each hotel type
 df_resort = df_filtered[df_filtered['hotel'] == 'Resort Hotel']
@@ -72,8 +68,10 @@ city_guests = df_city['arrival_month'].value_counts().sort_index()
 
 # Line chart for Resort and City
 fig3 = go.Figure()
-fig3.add_trace(go.Scatter(x=resort_guests.index, y=resort_guests.values, mode='lines+markers', name='Resort', line=dict(color='blue')))
-fig3.add_trace(go.Scatter(x=city_guests.index, y=city_guests.values, mode='lines+markers', name='City', line=dict(color='red')))
+fig3.add_trace(go.Scatter(x=resort_guests.index, y=resort_guests.values, mode='lines+markers', name='Resort',
+                          line=dict(color='blue')))
+fig3.add_trace(
+    go.Scatter(x=city_guests.index, y=city_guests.values, mode='lines+markers', name='City', line=dict(color='red')))
 fig3.update_layout(title_text="Total guests by month", xaxis_title="Month", yaxis_title="Number of Guests")
 
 # Filter data where 'is_canceled' is 1 (meaning the booking was cancelled)
@@ -113,28 +111,40 @@ fig5 = px.bar(temp_melt,
               labels={'market_segment': 'Market Segment'},
               color_discrete_sequence=["green", "blue"])
 
+# data = df[df['is_canceled'] == 0]
+df_filtered = df_filtered[df_filtered['is_canceled'] == 0]
+fig6 = px.box(data_frame=df_filtered, x='reserved_room_type', y='adr', color='hotel',
+              color_discrete_sequence=px.colors.qualitative.G10, title='Average Daily Rate by Room Type')
+fig6.update_layout(width=800, height=600)
+
 # Create buttons to toggle the visibility of the graphs
-button_labels = ['Hotel Type per Country', 'Total guests for Resort by month',
-                 'Total guests for City by month', 'Cancellation Counts by Distribution Channel',
-                 'Total guests for each hotel Grouped by market segment']
-button_values = [2, 3, 4]
+button_labels = ['Total guests for Resort and City hotel by month',
+                 'Cancellation Counts by Distribution Channel',
+                 'Total guests for each hotel Grouped by market segment',
+                 'Average Daily Rate by Room Type']
+
+button_values = [0, 1, 2, 3]
 selected_button = st.radio('Select a graph to display:', button_values, format_func=lambda x: button_labels[x])
 
-
-if selected_button == 2:
+if selected_button == 0:
     st.plotly_chart(fig3)
     st.markdown(
-        "The graph illustrates that the City Hotel consistently hosts a larger number of guests compared to the Resort Hotel. A notable trend across both establishments is the peak in guest numbers during August, indicating a high season. Conversely, the period from November to February appears to be a low season, with significantly fewer guests.")
+        "The goal of this graph is to compare the number of guests between the City Hotel and the Resort Hotel and identify the patterns and trends in guest numbers over time, The graph illustrates that the City Hotel consistently hosts a larger number of guests compared to the Resort Hotel. A notable trend across both establishments is the peak in guest numbers during August, indicating a high season. Conversely, the period from November to February appears to be a low season, with significantly fewer guests.")
     st.markdown("------------------")  # Add a horizontal line
-elif selected_button == 3:
+elif selected_button == 1:
     st.plotly_chart(fig4)
     st.markdown(
         "The graph demonstrates that the majority of cancellations are initiated via travel agencies or tour operators. Furthermore, it's evident that the City Hotel experiences a higher rate of cancellations compared to the Resort Hotel.")
     st.markdown("------------------")  # Add a horizontal line
-elif selected_button == 4:
+elif selected_button == 2:
     st.plotly_chart(fig5)
     st.markdown(
-        "This graph indicates that the predominant channel for bookings is through online travel agencies. Additionally, it's observable that the City Hotel garners more bookings across all market segments, with the exception of direct bookings, where the Resort Hotel takes the lead.")
+        "The goal of this graph is to provide insights into the market segments for the hotels. This graph indicates that the predominant channel for bookings is through online travel agencies. Additionally, it's observable that the City Hotel garners more bookings across all market segments, with the exception of direct bookings, where the Resort Hotel takes the lead.")
+    st.markdown("------------------")  # Add a horizontal line
+elif selected_button == 3:
+    st.plotly_chart(fig6)
+    st.markdown(
+        "The goal of this graph is to depict the relationship between the average price per room and its type. The figure shows that the average price per room depends on its type and the standard deviation.The graph aims to illustrate the popularity of different room types by showcasing the booking frequency. It also provides a comparison between the two hotel types based on room availability. The Average Daily Rate (ADR) is utilized as an index to indicate the average cost per night for reservations. This graph conveniently presents the ADR comparison across a range of diverse room types, offering valuable insights into pricing variations.")
     st.markdown("------------------")  # Add a horizontal line
 
 # Create the interactive plot
@@ -148,5 +158,6 @@ fig1 = px.choropleth(df_pivot, locations='country', color='total',
 
 st.plotly_chart(fig1)
 st.markdown(
-    "This choropleth map presents a comprehensive view of the geographical distribution of the hotel's guests by illustrating the total number of bookings per country. It's evident that Western Europe stands out with a higher volume of bookings compared to other regions. The prevalence of blue hues across the map signifies that reservations from these countries are relatively lower.")
+    "The goal of this graph is to provide a comprehensive overview of the regions with the highest order volumes. This choropleth map presents a comprehensive view of the geographical distribution of the hotel's guests by illustrating the total number of bookings per country. It's evident that Western Europe stands out with a higher volume of bookings compared to other regions. The prevalence of blue hues across the map signifies that reservations from these countries are relatively lower."
+    " the graph allows zooming in to obtain more precise and detailed information.")
 st.markdown("------------------")  # Add a horizontal line
